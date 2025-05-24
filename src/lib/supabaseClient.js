9ce6c1e4c62e93e86c9c7347240dd1177b89c868
alznzony <hamid132@hotmail.com>
2025-05-24 01:06:04 -0700

    Prepare project for GitHub: add env ignore and update supabase client

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://umrkxareviqvayfzmukr.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtcmt4YXJldmlxdmF5ZnptdWtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyODg0NjEsImV4cCI6MjA2Mjg2NDQ2MX0.tjS2XCWFOfvUqKQs0wS70MDDG4ew7RJJIBBI_I8M3P8";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
